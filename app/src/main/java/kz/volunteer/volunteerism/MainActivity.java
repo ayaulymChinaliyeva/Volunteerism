6b1887f2d929468cf178e4fe60586a5704dd1194
ayaulymChinaliyeva <37076155+ayaulymChinaliyeva@users.noreply.github.com>
2018-04-10 16:57:11 +0600

    Fixed my problems, now it's okay

--- app/src/main/java/kz/volunteer/volunteerism/MainActivity.java
+++ app/src/main/java/kz/volunteer/volunteerism/MainActivity.java
@@ -3,11 +3,30 @@ package kz.volunteer.volunteerism;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.aurelhubert.ahbottomnavigation.AHBottomNavigation;
+import com.stephentuso.welcome.WelcomeHelper;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
+    WelcomeHelper welcomeScreen;
+    @BindView(R.id.bottom_navigation)
+    AHBottomNavigation bottomNavigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        welcomeScreen = new WelcomeHelper(this, MyWelcomeActivity.class);
+        welcomeScreen.show(savedInstanceState);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        welcomeScreen.onSaveInstanceState(outState);
     }
 }
